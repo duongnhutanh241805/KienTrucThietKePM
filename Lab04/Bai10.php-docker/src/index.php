d161aeed0d7373f3a699eb1da62da1e5a98dd4d7
duongnhatanh <duongnhatanh241805@gmail.com>
2026-03-09 20:18:42 +0700

    lab04

---
+++ Lab04/Bai10.php-docker/src/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>Hello, Docker PHP!</h1>";
+echo "<p>Ứng dụng PHP chạy với Apache trong Docker.</p>";
+?>
---
+++ Lab04/Bai8.postgres-docker/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE fooddb;
+
+\c fooddb;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+INSERT INTO users (name, email) VALUES
+('Alice', 'alice@example.com'),
+('Bob', 'bob@example.com');
